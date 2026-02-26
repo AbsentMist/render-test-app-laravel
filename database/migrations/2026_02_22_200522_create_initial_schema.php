b5b93be62637c27ccaf508ac738380f3578090f7
Ngozoo <steven.ngoie@hes-so.ch>
2026-02-26 20:56:59 +0100

    configuration fichier de migration pour la base de données - SN

--- database/migrations/2026_02_22_200522_create_initial_schema.php
+++ database/migrations/2026_02_22_200522_create_initial_schema.php
@@ -44,9 +44,9 @@ return new class extends Migration
 
         Schema::create('Groupe', function (Blueprint $table) {
             $table->id();
-            $table->string('nom', 100);
-            $table->string('code_entreprise', 100)->nullable();
-            $table->string('type', 50); 
+            $table->string('nom', 100)->unique();
+            $table->string('code_entreprise', 100)->nullable()->unique();
+            $table->enum('type', ['Groupe' , 'Entreprise']);
         });
 
         Schema::create('Template', function (Blueprint $table) {
@@ -66,19 +66,14 @@ return new class extends Migration
 
         Schema::create('Options', function (Blueprint $table) {
             $table->id();
-            $table->string('nom', 80);
-            $table->string('type', 100);
+            $table->string('nom', 80)->unique();
+            $table->string('type', 100)->unique();
             $table->string('description', 255);
             $table->float('tarif')->default(0);
             $table->binary('image')->nullable();
         });
 
-        //Tables avec contraintes de clés étrangères
-        Schema::create('User', function (Blueprint $table) {
-            $table->id();
-            $table->string('email', 80)->unique();
-            $table->string('password', 100);
-        });
+        // Tables avec contraintes de clés étrangères
 
         Schema::create('Avertissement', function (Blueprint $table) {
             $table->id();
@@ -92,7 +87,7 @@ return new class extends Migration
             $table->foreignId('id_evenement')->constrained('Evenement')->onDelete('cascade');
             $table->foreignId('id_categorie')->nullable()->constrained('Categorie')->onDelete('set null'); //Enlever nullable une fois les catégories créées
             $table->foreignId('id_sous_categorie')->nullable()->constrained('SousCategorie')->onDelete('set null');
-            $table->string('nom', 120);
+            $table->string('nom', 120)->unique();
             $table->date('date');
             $table->date('debut_inscription');
             $table->date('fin_inscription');
@@ -109,12 +104,14 @@ return new class extends Migration
             $table->time('heure_fin')->nullable();
             $table->integer('age_minimum');
             $table->integer('age_maximum')->nullable();
+            $table->string('pop_info', 255)->nullable();
         });
 
         //Table d'association
         Schema::create('EvenementQuestion', function (Blueprint $table) {
             $table->foreignId('id_evenement')->constrained('Evenement')->onDelete('cascade');
             $table->foreignId('id_question')->constrained('Question')->onDelete('cascade');
+            $table->integer('ordre')->unique();
             $table->primary(['id_evenement', 'id_question']);
         });
 
@@ -131,42 +128,23 @@ return new class extends Migration
             $table->primary(['id_template', 'id_champ']);
         });
 
-        //Table d'association
+        //Table d'association (Lien vers la table users de l'auth)
         Schema::create('UserRole', function (Blueprint $table) {
             $table->foreignId('id_user')->constrained('User')->onDelete('cascade');
             $table->foreignId('id_role')->constrained('Role')->onDelete('cascade');
             $table->primary(['id_user', 'id_role']);
         });
 
+        // Lien vers la table users de l'auth
         Schema::create('Administrateur', function (Blueprint $table) {
             $table->id();
             $table->foreignId('id_user')->constrained('User')->onDelete('cascade');
         });
 
-        Schema::create('Participant', function (Blueprint $table) { //Attention : doublons avec autre fichier migration
-            $table->id();
-            $table->foreignId('id_user')->constrained('User')->onDelete('cascade');
-            $table->string('nom', 100);
-            $table->string('prenom', 100);
-            $table->date('date_naissance')->nullable();
-            $table->string('equipe_nom', 100)->nullable();
-            $table->string('adresse', 100)->nullable();
-            $table->string('code_postal', 10)->nullable();
-            $table->string('ville', 100)->nullable();
-            $table->string('pays', 100)->nullable();
-            $table->string('telephone', 20)->nullable();
-            $table->string('nationalite', 100)->nullable();
-            $table->string('instagram', 255)->nullable();
-            $table->string('facebook', 255)->nullable();
-            $table->string('taille_tshirt', 10)->nullable();
-            $table->string('sexe', 10)->nullable();
-            $table->binary('photo')->nullable();
-            $table->binary('banniere')->nullable();
-        });
-
+        // Lien vers la table participants de l'auth
         Schema::create('Document', function (Blueprint $table) {
             $table->id();
-            $table->string('url', 255);
+            $table->string('url', 255)->nullable();
             $table->date('date_debut')->nullable();
             $table->date('date_fin')->nullable();
             $table->boolean('valable')->default(0);
@@ -193,7 +171,7 @@ return new class extends Migration
             $table->boolean('is_coche')->default(0);
         });
 
-        //Table d'association
+        //Table d'association (Lien vers la table participants de l'auth)
         Schema::create('Inscription', function (Blueprint $table) {
             $table->id();
             $table->foreignId('id_participant')->constrained('Participant')->onDelete('cascade');
@@ -201,25 +179,25 @@ return new class extends Migration
             $table->foreignId('id_groupe')->nullable()->constrained('Groupe')->onDelete('set null');
             $table->foreignId('id_document')->nullable()->constrained('Document')->onDelete('set null');
             
-            $table->string('code_participant', 100)->nullable();
+            $table->string('code_participant', 100)->nullable()->unique();
             $table->float('tarif')->default(0);
-            $table->string('status_paiement', 50)->nullable();
-            $table->float('montant_rabais')->default(0);
-            $table->boolean('avertissement_valide')->default(0);
+            $table->enum('status_paiement', ['Validé', 'En attente', 'Annulé'])->default('En attente');
+            $table->float('montant_rabais')->default(0)->nullable();
+            $table->boolean('avertissement_valide')->default(0)->nullable();
         });
 
         Schema::create('Dossard', function (Blueprint $table) {
             $table->id();
             $table->integer('numero');
             $table->string('nom_personnalise', 150)->nullable();
-            $table->boolean('retrait_dossard')->default(0);
+            $table->boolean('retrait_dossard')->default(0)->nullable();
             $table->foreignId('id_inscription')->constrained('Inscription')->onDelete('cascade');
         });
 
         Schema::create('Resultat', function (Blueprint $table) {
             $table->id();
-            $table->time('temps_course')->nullable();
-            $table->integer('position')->nullable();
+            $table->time('temps_course')->nullable()->unique();
+            $table->integer('position');
             $table->foreignId('id_inscription')->constrained('Inscription')->onDelete('cascade');
         });
 
@@ -231,11 +209,11 @@ return new class extends Migration
             $table->foreignId('id_inscription')->constrained('Inscription')->onDelete('cascade');
         });
 
-        //Table d'association
+        //Table d'association (Lien vers la table participants de l'auth)
         Schema::create('GroupeParticipant', function (Blueprint $table) {
             $table->foreignId('id_groupe')->constrained('Groupe')->onDelete('cascade');
             $table->foreignId('id_participant')->constrained('Participant')->onDelete('cascade');
-            $table->string('statut', 50)->nullable();
+            $table->string('statut', 50);
             $table->primary(['id_groupe', 'id_participant']);
         });
 
@@ -250,9 +228,9 @@ return new class extends Migration
         $tables = [
             'GroupeParticipant', 'ReponseQuestion', 'Resultat', 'Dossard', 'Inscription',
             'OptionCochable', 'OptionQuantifiable', 'OptionPourCourse', 'Document', 
-            'Participant', 'Administrateur', 'UserRole', 'TemplatePersonnalise', 
-            'OptionQuestion', 'EvenementQuestion', 'Course', 'Avertissement', 'User',
-            'Option', 'Question', 'Champ', 'Template', 'Groupe', 'SousCategorie', 
+            'Administrateur', 'UserRole', 'TemplatePersonnalise', 
+            'OptionQuestion', 'EvenementQuestion', 'Course', 'Avertissement',
+            'Options', 'Question', 'Champ', 'Template', 'Groupe', 'SousCategorie', 
             'Categorie', 'Evenement', 'Role'
         ];
 
